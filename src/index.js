2aa70e82a85a5deced351b69ac33350fbde8c616
icebergnevercries <denislosik@mail.ru>
2018-10-02 21:44:02 +0300

    Implementation function, which solved sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  const arrayForSolves = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let len = arrayForSolves.length;
+  let condition = true;
+
+  function SearchElement(matrixSudoku){
+    for(let i = 0; i < len; i++){
+      for(let j = 0; j < len; j++){
+        if(matrixSudoku[i][j] == 0){
+          for(let k = 0; k < len; k++){
+            condition = true;
+            for(let n = 0; n < len; n++){
+              if(matrixSudoku[i][n] == arrayForSolves[k] || matrixSudoku[n][j] == arrayForSolves[k] || matrixSudoku[Math.floor(i / 3) * 3 + n % 3][Math.floor(j / 3) * 3 + Math.floor(n / 3)] == arrayForSolves[k]){
+                condition = false;
+              }
+            }
+            if(condition){
+              matrixSudoku[i][j] = arrayForSolves[k];
+              if(SearchElement(matrixSudoku)){
+                return true;
+              }
+              else{
+                matrixSudoku[i][j] = 0;
+              }
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  SearchElement(matrix);
+  return matrix;
 }
